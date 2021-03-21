5f00881afd5cb9c72139d18ea268788ed296fb25
arpitanshu <arpytanshu@gmail.com>
2021-03-21 15:21:06 +0530

    update scaling method for input covariates

--- data/data_loader.py
+++ data/data_loader.py
@@ -76,7 +76,8 @@ class Dataset_ETT_hour(Dataset):
         elif self.timeenc==1:
             data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
             data_stamp = data_stamp.transpose(1,0)
-
+        
+        data_stamp = data_stamp / data_stamp.max(axis=0)
         self.data_x = data[border1:border2]
         self.data_y = data[border1:border2]
         self.data_stamp = data_stamp
